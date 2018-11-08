d1298764987071f1367c9a516440bb3a2992ad34
Juan Morillo <jmorillos@uoc.edu>
2018-11-09 00:42:41 +0100

    Añadiendo imagen placeholder y DetailMovieViewController.

---
+++ TMDb/Controller/DetailMovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailMovieViewController.swift
+//  TMDb
+//
+//  Created by Jmorillo on 09/11/2018.
+//  Copyright © 2018 MorilloApps. All rights reserved.
+//
+
+import UIKit
+
+class DetailMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
